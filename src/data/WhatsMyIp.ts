a0eef7f3828b199177e22bc483466c354c544d40
Josh Glazebrook <josh@joshglazebrook.com>
2017-07-22 16:53:28 -0500

    Fixed issues with response options

--- src/data/WhatsMyIp.ts
+++ src/data/WhatsMyIp.ts
@@ -2,8 +2,6 @@
  * See: https://stat.ripe.net/docs/data_api#WhatsMyIp
  */
 
-import { BaseOptions } from './common/BaseOptions';
-
 export interface WhatsMyIpResponse {
   /**
    * The requesters ip address.
